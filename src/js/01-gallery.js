0454292c4e3eda4a7ea2ebda6f56722c843d3f05
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-09 22:11:15 +0200

    Додає файли

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,33 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
 
-// / Описаний в документації
+//? Описаний в документації
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
+//? Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 
+//!Пошук елементів
 const galleryEl = document.querySelector('.gallery');
 const imgMarkup = createsMarkupGalleryItems(galleryItems);
 
+//!Додавання рядка з HTML-тегами всередину елемента
 galleryEl.insertAdjacentHTML('beforeend', imgMarkup);
 
+//! Додавання затримки для опису зображення
 new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 
+//!Перебирання масиву об'єктів з файлу './gallery-items.js' та створення розмітки
 function createsMarkupGalleryItems(galleryItems) {
   return galleryItems
     .map(items => {
-      return `<div class="gallery__item">
+      return `
     <a class="gallery__link" href="${items.original}">
     <img
         class="gallery__image"
         src="${items.preview}"
         alt="${items.description}"
     />
-    </a>
-</div>`;
+    </a>`;
     })
     .join('');
 }
