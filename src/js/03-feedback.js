0cc24fbd4b636a425f20e8ee1b537f7b803a824d
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-13 21:11:08 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (formEl.email.value === '' || formEl.message.value === '') {
+    return alert('Заповни всі поля!');
+  }
+
+  const userInfo = {
+    email: formEl.email.value,
+    password: formEl.message.value,
+  };
+
+  console.log(userInfo);
+
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFormInput() {
+  const formData = {
+    email: formEl.email.value,
+    message: formEl.message.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+const savedData = localStorage.getItem(STORAGE_KEY);
+if (savedData) {
+  const formData = JSON.parse(savedData);
+  formEl.email.value = formData.email;
+  formEl.message.value = formData.message;
+}
