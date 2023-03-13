cc6e97a03309a1a4c47f5938e5440295bcdf8073
AlenaBrusnikina <115583047+AlenaBrusnikina@users.noreply.github.com>
2023-03-13 18:13:42 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,18 @@
-import Player from "@vimeo/player";
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
 
+player.on(
+  'timeupdate',
+  throttle(e => {
+    localStorage.setItem(LOCALSTORAGE_KEY, e.seconds);
+  }, 1000)
+);
+
+const pauseTime = localStorage.getItem(LOCALSTORAGE_KEY);
+if (pauseTime) {
+  player.setCurrentTime(pauseTime);
+}
